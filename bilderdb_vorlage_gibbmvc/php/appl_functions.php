a645d69d8ab2dbcad440c4e3be4778fceb80d270
nickweyermann <20nickw00@gmail.com>
2018-03-15 09:09:06 +0100

    appl_functions added validation, added registration view

--- bilderdb_vorlage_gibbmvc/php/appl_functions.php
+++ bilderdb_vorlage_gibbmvc/php/appl_functions.php
@@ -18,5 +18,29 @@ function login() {
  * Beinhaltet die Anwendungslogik zur Registration
  */
 function registration() {
+	setValue("phpmodule", $_SERVER['PHP_SELF']."?id=".getValue("func"));
+	return runTemplate( "../templates/".getValue("func").".htm.php" );
+
+	if (isset($_POST["registrieren"])) {
+		$fehlermeldung = checkMail();
+		// Wenn ein Fehler aufgetreten ist
+		if (strlen($fehlermeldung) > 0) {
+			setValue("meldung", $fehlermeldung);
+			setValues($_POST);
+		} else {
+			return header("Location: ".$_SERVER['PHP_SELF']."?id=login");
+		}
+	}
+}
+function checkMail() {
+  $fehlermeldung = "";
+
+  if (!checkEmpty($_POST['username'], 3)) {
+	$fehlermeldung .= "Der Benutzername muss mind. 3 Zeichen lang sein. ";
+  }
+  if (!checkEmail($_POST['email'])) {
+	$fehlermeldung .= "Falsches Format E-Mail. ";
+  }
+  return $fehlermeldung;
 }
 ?>
--- bilderdb_vorlage_gibbmvc/templates/registration.htm.php
+++ bilderdb_vorlage_gibbmvc/templates/registration.htm.php
@@ -1,6 +1,12 @@
 <div class="col-md-12">
 <form name="kontakt" class="form-horizontal form-condensed" action="" method="post">
   <div class="form-group control-group">
+	<label class="control-label col-md-offset-2 col-md-2" for="username">Benutzername</label>
+	<div class="col-md-4">
+	  <input type="name" class="form-control" id="username" name="username" value="" />
+	</div>
+  </div>
+  <div class="form-group control-group">
 	<label class="control-label col-md-offset-2 col-md-2" for="email">E-Mail</label>
 	<div class="col-md-4">
 	  <input type="email" class="form-control" id="email" name="email" value="" />
@@ -13,8 +19,22 @@
 	</div>
   </div>
   <div class="form-group control-group">
+	<label class="control-label col-md-offset-2 col-md-2" for="password2">Passwort wiederholen</label>
+	<div class="col-md-4">
+	  <input type="password" class="form-control" id="password2" name="password2" value="" />
+	</div>
+  </div>
+  <div class="form-group control-group">
 	<div class="col-md-offset-4 col-md-4">
-	  <button type="submit" class="btn btn-success" name="login" id="login">login</button>
+	  <button type="submit" class="btn btn-success" name="registrieren" id="registrieren">Registrieren</button>
+	</div>
+  </div>
+  <div class="form-group control-group">
+	<div class="col-md-offset-4 col-md-4">
+	<?php
+		$meldung = getValue("meldung");
+		if (strlen($meldung) > 0) echo "<div class='col-md-offset-2 col-md-6 alert alert-danger'>$meldung</div>";
+	?>
 	</div>
   </div>
 </form>
