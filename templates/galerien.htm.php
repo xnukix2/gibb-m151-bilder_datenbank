c4c15e4013ca2db4b6fcf1145334d1ccc9c94374
nickweyermann <20nickw00@gmail.com>
2018-03-28 09:30:43 +0200

    added dynamic slideshow + working image upload

--- templates/galerien.htm.php
+++ templates/galerien.htm.php
@@ -1,3 +1,8 @@
+<?php
+  	$sql = "SELECT bilderID FROM bilder ORDER BY bilderID DESC"; 
+	$result = mysqli_query(getValue("cfg_db"), $sql);
+?>
+
 <div class="col-md-12">
 <form name="galerien" class="form-horizontal form-condensed" action="" method="post">
   <div class="form-group control-group">
@@ -12,18 +17,27 @@
 		  <div class="carousel-inner">
 
 		  	<?php
-		  	$sql = "SELECT bilderID FROM bilder ORDER BY bilderID DESC"; 
-  			$result = mysqli_query(getValue("cfg_db"), $sql);
-		  	while($row = $result->fetch_assoc()) {
+	  			$count = 0;
+			  	while($row = $result->fetch_assoc()) {
+			  		if ($count == 0) {
+				  		?>
+						<div class="item active">
+				    		<img src="imageView.php?image_id=<?php echo $row["bilderID"]; ?>">
+				    	</div>
+						<?php
+						$count += 1;
+			  		}
+			  		else {
+					?>
+					<div class="item">
+						<img src="imageView.php?image_id=<?php echo $row["bilderID"]; ?>">
+					</div>
+					<?php
+					}
+				}
 			?>
-			<div class="item">
-				<img src="imageView.php?image_id=<?php echo $row["imageID"]; ?>" />
-			</div>
-			<?php		
-			}
-			?>
-		    <div class="item active">
-		      <img src="galaxy1.jpg" width="1100" height="500">
+		    <div class="item">
+		      <img src="download.png">
 		    </div>
 		  </div>
 		  
@@ -41,10 +55,6 @@
 <?php
 	$meldung = getValue("meldung");
 	if (strlen($meldung) > 0) echo "<div class='col-md-offset-2 col-md-6 alert alert-danger'>$meldung</div>";
-
-	echo "<div class='col-md-offset-2 col-md-4 carousel'>
-	<img src='".getValue('bilder')."'>
-	</div>";
 
 	if (isset($_SESSION['session'])) {
 		echo "<div class='loginbox'>Member: ".$_SESSION['session']."</div>";
